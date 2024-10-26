b195a65184545fd0661c7892db9aee3d85744072
Nhel <141327891+nhlhrnndz@users.noreply.github.com>
2024-10-25 15:56:24 -1200

    add the attributes and classes

--- History.java
+++ History.java
@@ -1,3 +1,17 @@
 public class History {
-    
+
+    private String Location, ClientSatus;
+    private int HouseNumber, ContactNumber, PrevReading, CurrentReading, BalancethisMonth, CurrentBalance;
+
+    public void StoreClientData(){
+
+    }
+
+    public void StoreBillData(){
+
+    }
+
+    public void StorePaymentData(){
+
+    }
 }
--- Payment.java
+++ Payment.java
@@ -1,3 +1,14 @@
 public class Payment {
+
+    private String PaymentMethod;
+    private int HouseNumber, PaymentDate, BalancethisMonth, InputPayment, CurrentBalance;
+
+    public void payOnline(){
+
+    }
+
+    public void generateReceipt(){
+        
+    }
     
 }
