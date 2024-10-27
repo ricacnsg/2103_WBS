dc25b9e4235eb32b1e071f34cc2d1eee18014684
Jason Manuel <villaflorjasonmanuel22@gmail.com>
2024-10-27 18:34:27 +0800

    Update Adminclass.java

---
+++ Adminclass.java
@@ -0,0 +1,41 @@
+public class Admin {
+    private String Name, username, password;
+    private int AdminID, ContactInfo;
+
+    public void setUsername(){
+
+    }
+
+    public void getUsername(){
+
+    }
+
+    public void setPassword(){
+
+    }
+
+    public void getPassword(){
+
+    }
+
+    public void setAdminID(){
+
+    }
+    
+    public void getAdminID(){
+
+    }
+
+    public void Login(){
+
+    }
+
+    public void isAdminValid(){
+
+    }
+
+    public void retrieveHistoryData(){
+
+    }
+
+}
